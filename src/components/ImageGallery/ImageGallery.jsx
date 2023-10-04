9cd47d209e21e63d2d02b243acac5c0748e01d96
ndnastia <ndnastiadatsiuk@gmail.com>
2023-10-04 08:15:27 +0200

    fixed

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,6 @@
 import styles from './ImageGallery.module.css';
 import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem";
-import Modal from 'components/Modal/Modal';
+
 
 
 export const ImageGallery = ({ images}) => {
